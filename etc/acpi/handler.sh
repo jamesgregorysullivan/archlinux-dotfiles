ab9ae595d6462457f0ccce84d521a1f0bedee5b9
Jakub Klinkovský <kuba.klinkovsky@gmail.com>
2012-09-13 15:42:24 +0200

    move multimedia keys handling from acpi to openbox

--- etc/acpi/handler.sh
+++ etc/acpi/handler.sh
@@ -1,30 +1,6 @@
 #!/bin/sh
 
-DBUS=$(ps aux | grep 'dbus-launch' | grep -v root)
-if [[ ! -z $DBUS ]];then
-    USER=$(echo $DBUS | awk '{print $1}')
-    USERHOME=$(getent passwd $USER | cut -d: -f6)
-    export XAUTHORITY="$USERHOME/.Xauthority"
-    for x in /tmp/.X11-unix/*; do
-        DISPLAYNUM=$(echo $x | sed s#/tmp/.X11-unix/X##)
-        if [[ -f "$XAUTHORITY" ]]; then
-            export DISPLAY=":$DISPLAYNUM"
-        fi
-    done
-fi
-
 case "$1" in
-    hotkey)
-        case "$2" in
-            ATK0100:00)
-                case "$3" in
-                    0000006b)
-                        /etc/acpi/actions/toggle_touchpad.sh
-                        ;;
-                esac
-                ;;
-        esac
-        ;;
     button*)
         case "$2" in
             LID)
@@ -35,32 +11,6 @@ case "$1" in
             SCRNLCK)
                 systemctl start slimlock.service
                 systemctl start tmuxlock.service
-                ;;
-#            WLAN)
-#                systemctl start rfkill.service
-#                ;;
-        esac
-        ;;
-    cd*)
-        case "$2" in
-            CDPLAY)
-                mpc toggle
-                ;;
-            CDSTOP)
-                mpc stop
-                ;;
-            CDPREV)
-                mpc prev
-                ;;
-            CDNEXT)
-                mpc next
-                ;;
-        esac
-        ;;
-    video*)
-        case "$2" in
-            BRTUP|BRTDN)
-                su lahwaacz -c "/etc/acpi/actions/display-brightness-notify.py"
                 ;;
         esac
         ;;
